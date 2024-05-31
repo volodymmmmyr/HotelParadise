57ba518f72ed7c73cc2ed38dc100929a1df30cf2
volodymmmmyr <vdidkiv@knu.ua>
2024-06-01 01:51:38 +0300

    add admin booking

--- hotel_paradise/contact/admin.py
+++ hotel_paradise/contact/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Booking
 
-# Register your models here.
+@admin.register(Booking)
+class BookingAdmin(admin.ModelAdmin):
+    list_display = ('id', 'guest_name', 'guest_email', 'room', 'check_in', 'check_out', 'special_request', 'total_price')
+    list_editable = ('guest_name', 'guest_email', 'room', 'check_in', 'check_out', 'special_request', 'total_price')
+    list_filter = ('check_in', 'check_out', 'room', 'total_price')
+    search_fields = ('guest_name', 'guest_email', 'room', 'check_in', 'check_out')
