f7a4307e4897aa8e896d9f0a2f77970f218b7dbe
volodymmmmyr <vdidkiv@knu.ua>
2024-06-01 01:51:01 +0300

    add model of Booking model

--- hotel_paradise/contact/models.py
+++ hotel_paradise/contact/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from room.models import Room
+import unittest
 
-# Create your models here.
+class Booking(models.Model):
+    guest_name = models.CharField(max_length=100)
+    guest_email = models.EmailField()
+    check_in = models.DateField()
+    check_out = models.DateField()
+    room = models.ForeignKey(Room, on_delete=models.CASCADE)
+    special_request = models.CharField(max_length=300)
+    total_price = models.DecimalField(max_digits=10, decimal_places=2)
+
+    def __str__(self):
+        return f"{self.guest_name}, {self.room.title}, {self.check_in} to {self.check_out}, ${self.total_price}"
+
