94b8aa55f3e4e95ef2c6499b8edfd69b3365ba26
volodymmmmyr <vdidkiv@knu.ua>
2024-06-01 01:57:31 +0300

    add Room Model for room

--- hotel_paradise/room/models.py
+++ hotel_paradise/room/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+
+class Room(models.Model):
+    title = models.CharField(max_length=40, default="")
+    bedroom_count = models.IntegerField()
+    bathroom_count = models.IntegerField()
+    price_per_night = models.DecimalField(max_digits=10, decimal_places=2)
+    description = models.TextField()
+    image = models.ImageField(upload_to="static/img/room_images/")
+    available = models.BooleanField(default=True)
+
+    def __str__(self):
+        return f"{self.title}, {self.bedroom_count} Bedroom, {self.bathroom_count} Bathroom, ${self.price_per_night}"
+
