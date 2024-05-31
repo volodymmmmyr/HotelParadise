51cd45f8761e776c70e9b90bce9008fe86a15b0e
volodymmmmyr <vdidkiv@knu.ua>
2024-06-01 01:57:10 +0300

    add admin RoomAdmin

--- hotel_paradise/room/admin.py
+++ hotel_paradise/room/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .models import Room
 
-# Register your models here.
+@admin.register(Room)
+class RoomAdmin(admin.ModelAdmin):
+    list_display = ('id', 'title', 'bedroom_count', 'bathroom_count', 'price_per_night', 'description', 'image', 'available')
+    list_editable = ('title', 'bedroom_count', 'bathroom_count', 'price_per_night', 'description', 'image', 'available')
+    list_filter = ('price_per_night', 'bedroom_count', 'available')
+    search_fields = ('bedroom_count', 'bathroom_count', 'price_per_night', 'available')
+
+
+
