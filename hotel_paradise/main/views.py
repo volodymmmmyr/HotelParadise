a8c198ae563fccfc70dc265ee53f2a3746abca52
volodymmmmyr <vdidkiv@knu.ua>
2024-06-01 01:56:18 +0300

    add view for main

--- hotel_paradise/main/views.py
+++ hotel_paradise/main/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
-
+from room.models import Room
 # Create your views here.
+
+def home(request):
+    if Room.objects.filter(available=True).exists():
+        rooms = Room.objects.filter(available=True)
+    else: rooms = Room.objects.all()
+    
+    return render(request, "main.html", context={
+        "rooms": rooms,
+    })
