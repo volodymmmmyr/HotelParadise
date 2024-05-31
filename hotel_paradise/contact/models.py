cd2d07c81bf6d89f642682aa7131549e35a2daba
volodymmmmyr <vdidkiv@knu.ua>
2024-06-01 01:54:28 +0300

    add mode of contact and correction for models in booking

--- hotel_paradise/contact/models.py
+++ hotel_paradise/contact/models.py
@@ -1,16 +1,11 @@
 from django.db import models
-from room.models import Room
-import unittest
 
-class Booking(models.Model):
-    guest_name = models.CharField(max_length=100)
-    guest_email = models.EmailField()
-    check_in = models.DateField()
-    check_out = models.DateField()
-    room = models.ForeignKey(Room, on_delete=models.CASCADE)
-    special_request = models.CharField(max_length=300)
-    total_price = models.DecimalField(max_digits=10, decimal_places=2)
+
+class Contact(models.Model):
+    name = models.CharField(max_length=100)
+    email = models.EmailField()
+    subject = models.CharField(max_length=50)
+    message = models.TextField()
 
     def __str__(self):
-        return f"{self.guest_name}, {self.room.title}, {self.check_in} to {self.check_out}, ${self.total_price}"
-
+        return f"{self.name} - {self.email} - {self.subject}"
